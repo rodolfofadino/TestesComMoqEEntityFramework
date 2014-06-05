67d6e8a5909e85cb7524fd01771f2208b45ffa1e
Rodolfo <rodolfo@minhavida.com.br>
2014-06-05 11:43:24 -0300

    adicao da parte web do projeto

---
+++ ProjetoLivro.Web/Controllers/LivroController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoLivro.BL;
+
+namespace ProjetoLivro.Web.Controllers
+{
+    public class LivroController : Controller
+    {
+        //Nao façam isso!!!
+        private LivroService livroService = new LivroService(new LivroContext());
+
+        public ActionResult Index()
+        {
+            return View(livroService.GetLivrosAtivos());
+        }
+
+        // GET: /Livro/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Status")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                livroService.Salvar(livro);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(livro);
+        }
+    }
+}
