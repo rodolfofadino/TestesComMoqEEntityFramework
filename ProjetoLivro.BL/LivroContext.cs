69afa09a049a6ae99f2f1e9ed97dcf2888412e63
Rodolfo <rodolfo@minhavida.com.br>
2014-06-05 11:16:23 -0300

    adicao das classes que manipulam o livro, e referencia do EntityFramework no projeto

---
+++ ProjetoLivro.BL/LivroContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLivro.BL
+{
+    public class LivroContext : DbContext
+    {
+        public virtual DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ ProjetoLivro.BL/LivroService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLivro.BL
+{
+    public class LivroService
+    {
+        private LivroContext _context { get; set; }
+        public LivroService(LivroContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> GetLivrosAtivos()
+        {
+            return _context.Livros.Where(a => a.Status == true).ToList();
+        }
+
+        public void Salvar(Livro livro)
+        {
+            _context.Livros.Add(livro);
+            _context.SaveChanges();
+        }
+    }
+}
